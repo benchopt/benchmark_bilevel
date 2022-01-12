40c93fa4d39a571f1a8619e95e84248e5c29ef09
MatDag <mathieu.dagreou@inria.fr>
2022-01-12 15:25:27 +0100

    FIX computing the right outer direction in fsla

--- solvers/fsla.py
+++ solvers/fsla.py
@@ -63,12 +63,15 @@ class Solver(BaseSolver):
         v = np.zeros_like(inner_var)
         d = np.zeros_like(outer_var)
 
+        memory_inner = np.zeros_like(inner_var)
+        memory_outer = np.zeros_like(outer_var)
+
         while callback((inner_var, outer_var)):
             inner_var, outer_var, v, d = fsla(
                 self.f_inner.numba_oracle, self.f_outer.numba_oracle,
-                inner_var, outer_var, v, d, eval_freq,
-                inner_sampler, outer_sampler, inner_step_size, outer_step_size,
-                eta, seed=rng.randint(constants.MAX_SEED)
+                inner_var, outer_var, v, d, memory_inner, memory_outer,
+                eval_freq, inner_sampler, outer_sampler, inner_step_size,
+                outer_step_size, eta, seed=rng.randint(constants.MAX_SEED)
             )
         self.beta = (inner_var, outer_var)
 
@@ -77,8 +80,8 @@ class Solver(BaseSolver):
 
 
 @njit()
-def fsla(inner_oracle, outer_oracle, inner_var, outer_var, v, memory_outer,
-         max_iter, inner_sampler, outer_sampler,
+def fsla(inner_oracle, outer_oracle, inner_var, outer_var, v, d, memory_inner,
+         memory_outer, max_iter, inner_sampler, outer_sampler,
          inner_step_size, outer_step_size, eta, seed=None):
     # Set seed for randomness
     np.random.seed(seed)
@@ -98,6 +101,7 @@ def fsla(inner_oracle, outer_oracle, inner_var, outer_var, v, memory_outer,
         grad_outer = outer_oracle.grad_inner_var(
             inner_var, outer_var, slice_outer
         )
+        v_old = v.copy()
         v -= inner_step_size * (hvp - grad_outer)
 
         # Step.3 - compute the implicit gradient estimate
@@ -105,15 +109,26 @@ def fsla(inner_oracle, outer_oracle, inner_var, outer_var, v, memory_outer,
         impl_grad = outer_oracle.grad_outer_var(
             inner_var, outer_var, slice_outer2
         )
+        impl_grad_old = outer_oracle.grad_outer_var(
+            memory_inner, memory_outer, slice_outer2
+        )
         slice_inner3, _ = inner_sampler.get_batch(inner_oracle)
         impl_grad -= inner_oracle.cross(inner_var, outer_var, v, slice_inner3)
+        impl_grad_old -= inner_oracle.cross(
+            memory_inner, memory_outer, v_old, slice_inner3
+        )
 
         # Step.4 - update direction with momentum
-        memory_outer += eta * (impl_grad - memory_outer)
+        d = impl_grad + (1-eta) * (d - impl_grad_old)
 
         # Step.5 - update the outer variable
-        outer_var -= outer_step_size * memory_outer
+        outer_var -= outer_step_size * d
 
         # Step.6 - project back to the constraint set
         inner_var, outer_var = inner_oracle.prox(inner_var, outer_var)
-    return inner_var, outer_var, v, memory_outer
+
+        # Step.7 - update memories
+        memory_inner = inner_var
+        memory_outer = outer_var
+
+    return inner_var, outer_var, v, d
