ec2a047e2c59f2df9d10fd83ac283dad6e37708a
MatDag <mathieu.dagreou@inria.fr>
2022-05-03 14:40:38 +0200

    WIP amigo 1 step in z and 100 steps in v

--- utils/constants.py
+++ utils/constants.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-EVAL_FREQ = 2 ** 1
+EVAL_FREQ = 2 ** 3
 PATIENCE = 400000
 
 STEP_SIZES = [1]
@@ -8,8 +8,8 @@ OUTER_RATIOS = [.01]
 BATCH_SIZES = ['full']
 
 # Number of inner loop steps
-N_INNER_STEPS = [10]
-N_V_STEPS = [10]
+N_INNER_STEPS = [1]
+N_V_STEPS = [100]
 N_HIA_STEPS = [3]
 
 # STORM momentum parameter
