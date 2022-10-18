58d6f46bb7fb32b9381186f06929f7af6299d399
MatDag <mathieu.dagreou@inria.fr>
2022-10-18 08:16:24 +0200

    FIX pep8

--- datasets/simulated.py
+++ datasets/simulated.py
@@ -18,7 +18,7 @@ class Dataset(BaseDataset):
         'n_samples, n_features': [
             (1000, 10),
         ],
-        'correlation': [0, .5, 1-1e-10, 1-1e-15]
+        'correlation': [0, .5, 1 - 1e-10, 1 - 1e-15]
 
     }
 
