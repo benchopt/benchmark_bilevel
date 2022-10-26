12567e76ea474c85b1f5481e72d8aafe0d46a4b3
MatDag <mathieu.dagreou@inria.fr>
2022-10-26 17:25:09 +0200

    FIX rm MultinomialLogRegOracle in utils.oracles.__init__

--- utils/oracles/__init__.py
+++ utils/oracles/__init__.py
@@ -1,9 +1,7 @@
 from .ridge import RidgeRegressionOracle
 from .logreg import LogisticRegressionOracle
 from .datacleaning import DataCleaningOracle
-from .multinomial_regression import MultinomialLogRegOracle
 from .multi_logreg import MultiLogRegOracle
 
 __all__ = ['RidgeRegressionOracle', 'LogisticRegressionOracle',
-           'DataCleaningOracle', 'MultinomialLogRegOracle',
-           'MultiLogRegOracle']
+           'DataCleaningOracle', 'MultiLogRegOracle']
