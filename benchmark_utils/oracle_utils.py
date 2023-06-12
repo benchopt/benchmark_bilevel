00c102af267ac729f8d8c3b267798aca73a3cfbf
MatDag <mathieu.dagreou@inria.fr>
2023-06-12 16:42:52 +0200

    ENH get_oracle in oracle

--- benchmark_utils/oracle_utils.py
+++ benchmark_utils/oracle_utils.py
@@ -1,21 +1,6 @@
 import jax.numpy as jnp
 
 
-def get_oracle(OracleClass, *args, framework="none", get_fb=False,
-               **kwargs):
-    oracle = OracleClass(*args, **kwargs)
-    if framework == "numba":
-        oracle = oracle.numba_oracle
-    elif framework == "jax":
-        if get_fb:
-            oracle = oracle.jax_oracle, oracle.jax_oracle_fb
-        else:
-            oracle = oracle.jax_oracle
-    elif framework != "none":
-        raise ValueError(f"Framework {framework} not supported.")
-    return oracle
-
-
 def convert_array_framework(x, framework=None):
     if framework == "jax":
         x = jnp.array(x)
