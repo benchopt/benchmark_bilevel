31b3e07d7b2f90135db27cea881e167ad16b1a1c
MatDag <mathieu.dagreou@inria.fr>
2023-06-12 16:43:24 +0200

    FIX adapt from sampler rfc

--- tests/test_sampler.py
+++ tests/test_sampler.py
@@ -7,8 +7,8 @@ from benchmark_utils.minibatch_sampler import init_sampler  # noqa: E402
 
 
 @pytest.mark.parametrize('n_samples', [100])
-@pytest.mark.parametrize('batch_size', [20])
+@pytest.mark.parametrize('batch_size', [23])
 def test_lr_scheduler(n_samples, batch_size):
     sampler, state_sampler = init_sampler(n_samples, batch_size)
     for k in range(10):
-        selector, state_sampler = sampler(**state_sampler)
+        selector, _, _, state_sampler = sampler(state_sampler)
