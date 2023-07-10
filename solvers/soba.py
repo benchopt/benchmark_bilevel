a628a42bb7cec6b9186113427ef6057efd431bf9
MatDag <mathieu.dagreou@inria.fr>
2023-07-10 11:56:05 +0200

    FIX flake8 docstring SOBA

--- solvers/soba.py
+++ solvers/soba.py
@@ -25,7 +25,9 @@ with safe_import_context() as import_ctx:
 
 class Solver(BaseSolver):
     """Stochastic Bilevel Algorithm (SOBA).
-    M. Dagréou, P. Ablin, S. Vaiter and T. Moreau, "A framework for bilevel optimization that enables stochastic and global variance reduction algorithms", NeurIPS 2022."""
+    M. Dagréou, P. Ablin, S. Vaiter and T. Moreau, "A framework for bilevel
+    optimization that enables stochastic and global variance reduction
+    algorithms", NeurIPS 2022."""
     name = 'SOBA'
 
     stopping_criterion = SufficientProgressCriterion(
