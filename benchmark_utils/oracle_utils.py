367ec657a40df6aafae3f42c87cfb12164b9a663
MatDag <mathieu.dagreou@inria.fr>
2023-08-04 14:01:30 +0200

    FIX avoid memory issue when creating jax sparse matrices

--- benchmark_utils/oracle_utils.py
+++ benchmark_utils/oracle_utils.py
@@ -10,7 +10,18 @@ def convert_array_framework(x, framework=None):
             # Passing by a dense matrix is mandatory to be able to use
             # n_batch>1. n_batch = 0 is incompatible with vmapping for the
             # moment.
-            x = sparse.BCOO.fromdense(x.todense(), n_batch=1)
+            nb = 1000
+            temp = sparse.BCOO.fromdense(x[:nb].todense(), n_batch=1)
+            for i in range((x.shape[0] + nb - 1) // nb - 1):
+                temp = sparse.bcoo_concatenate(
+                    [
+                        temp,
+                        sparse.BCOO.fromdense(x[(i+1)*nb:(i+2)*nb].todense(),
+                                              n_batch=1)
+                    ],
+                    dimension=0
+                )
+            x = temp
         else:
             x = jnp.array(x)
     return x
