a5e27e1e1460891811aba6587abb9c533eb017e7
Pierre Ablin <pierreablin@gmail.com>
2024-06-04 12:14:09 +0200

    update

--- objective.py
+++ objective.py
@@ -20,20 +20,37 @@ class Objective(BaseObjective):
         self.random_state = random_state
 
     def get_one_result(self):
-        inner_shape, outer_shape = self.dim_inner, self.dim_outer
+        inner_shape, outer_shape = (
+            self.dim_inner,
+            self.dim_outer,
+        )
         return dict(
             inner_var=jax.tree_util.tree_map(
-                jnp.zeros, inner_shape, is_leaf=lambda x: isinstance(x, tuple)
+                jnp.zeros,
+                inner_shape,
+                is_leaf=lambda x: isinstance(x, tuple),
             ),  # can be a pytree
             outer_var=jax.tree_util.tree_map(
-                jnp.zeros, outer_shape, is_leaf=lambda x: isinstance(x, tuple)
+                jnp.zeros,
+                outer_shape,
+                is_leaf=lambda x: isinstance(x, tuple),
             ),
         )
 
     def set_data(self, pb_inner, pb_outer, metrics, init_var=None):
 
-        (self.f_inner, self.n_samples_inner, self.dim_inner, self.f_inner_fb) = pb_inner
-        (self.f_outer, self.n_samples_outer, self.dim_outer, self.f_outer_fb) = pb_outer
+        (
+            self.f_inner,
+            self.n_samples_inner,
+            self.dim_inner,
+            self.f_inner_fb,
+        ) = pb_inner
+        (
+            self.f_outer,
+            self.n_samples_outer,
+            self.dim_outer,
+            self.f_outer_fb,
+        ) = pb_outer
         self.metrics = metrics
 
         key = jax.random.PRNGKey(self.random_state)
@@ -42,7 +59,9 @@ class Objective(BaseObjective):
             self.inner_var0, self.outer_var0 = init_var(key)
         else:
             self.inner_var0 = jax.tree_util.tree_map(
-                jnp.zeros, self.dim_inner, is_leaf=lambda x: isinstance(x, tuple)
+                jnp.zeros,
+                self.dim_inner,
+                is_leaf=lambda x: isinstance(x, tuple),
             )
             self.outer_var0 = jax.tree_util.tree_map(
                 lambda x: -2 * jnp.ones(x),
