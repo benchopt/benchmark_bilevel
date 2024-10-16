1f47feb44cb56d83d23912a75c4387c7b12fb7f6
MatDag <mathieu.dagreou@inria.fr>
2024-10-16 15:57:33 +0200

    WIP comment cnn

--- datasets/distillation.py
+++ datasets/distillation.py
@@ -56,15 +56,15 @@ class CNN(nn.Module):
     """A simple CNN model."""
     @nn.compact
     def __call__(self, x):
-        x = nn.Conv(features=32, kernel_size=(3, 3))(x)
-        x = nn.relu(x)
-        x = nn.avg_pool(x, window_shape=(2, 2), strides=(2, 2))
-        x = nn.Conv(features=64, kernel_size=(3, 3))(x)
-        x = nn.relu(x)
-        x = nn.avg_pool(x, window_shape=(2, 2), strides=(2, 2))
-        x = x.reshape((x.shape[0], -1))  # flatten
-        x = nn.Dense(features=256)(x)
-        x = nn.relu(x)
+        # x = nn.Conv(features=32, kernel_size=(3, 3))(x)
+        # x = nn.relu(x)
+        # x = nn.avg_pool(x, window_shape=(2, 2), strides=(2, 2))
+        # x = nn.Conv(features=64, kernel_size=(3, 3))(x)
+        # x = nn.relu(x)
+        # x = nn.avg_pool(x, window_shape=(2, 2), strides=(2, 2))
+        # x = x.reshape((x.shape[0], -1))  # flatten
+        # x = nn.Dense(features=256)(x)
+        # x = nn.relu(x)
         x = nn.Dense(features=10)(x.reshape((x.shape[0], -1)))
         return x
 
