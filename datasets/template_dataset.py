59001b3da70c1057adea54149bf3de725e2f8b8e
MatDag <mathieu.dagreou@inria.fr>
2024-10-18 13:53:04 +0200

    WIP

--- datasets/template_dataset.py
+++ datasets/template_dataset.py
@@ -6,7 +6,6 @@ from benchopt import safe_import_context
 # - getting requirements info when all dependencies are not installed.
 with safe_import_context() as import_ctx:
     import numpy as np
-    from libsvmdata import fetch_libsvm
 
     import jax
     import jax.numpy as jnp
@@ -15,37 +14,65 @@ with safe_import_context() as import_ctx:
     from jaxopt import LBFGS
 
 
-def loss_sample(inner_var, outer_var, x, y):
-    return -jax.nn.log_sigmoid(y*jnp.dot(inner_var, x))
+def generate_matrices(dim_inner, dim_outer, key=jax.random.PRNGKey(0)):
+    """Generates the different matrices of the inner and outer quadratic
+    functions."""
+    keys = jax.random.split(key, 4)
+    eig_inner = jnp.logpsace(-1, 0, dim_inner)
+    eig_outer = jnp.logpsace(-1, 0, dim_inner)
+    eig_cross = jnp.logpsace(-1, 0, min(dim_inner, dim_outer))
+
+    # Matrix generation for the inner function
+    # Generate a PSD matrix with eigenvalues `eig_inner`
+    hess_inner_inner = jax.random.normal(keys[0], (dim_inner, dim_inner))
+    U, _, _ = jnp.linalg.svd(hess_inner_inner)
+    hess_inner_inner = U @ jnp.diag(eig_inner) @ U.T
+
+    # Generate a PSD matrix with eigenvalues `eig_outer`
+    hess_outer_inner = jax.random.normal(keys[1], (dim_outer, dim_outer))
+    U, _, _ = jnp.linalg.svd(hess_outer_inner)
+    hess_outer_inner = U @ jnp.diag(eig_outer) @ U.T
+
+    # Generate a PSD matrix with eigenvalues `eig_outer`
+    cross_inner = jax.random.normal(keys[2], (dim_outer, dim_inner))
+    D = jnp.zeros((dim_outer, dim_inner))
+    D = D.at[:min(dim_outer, dim_inner), :min(dim_outer, dim_inner)].set(
+        jnp.diag(eig_cross)
+    )
+    U, _, V = jnp.linalg.svd(cross_inner)
+    cross_inner = U @ D @ V.T
+
+    hess_inner_outer = jax.random.normal(keys[3], (dim_inner, dim_inner))
+    U, _, _ = jnp.linalg.svd(hess_inner_outer)
+    hess_inner_outer = U @ jnp.diag(eig_inner) @ U.T
+
+    return hess_inner_inner, hess_outer_inner, cross_inner, hess_inner_outer
 
 
-def loss(inner_var, outer_var, X, y):
-    batched_loss = jax.vmap(loss_sample, in_axes=(None, None, 0, 0))
-    return jnp.mean(batched_loss(inner_var, outer_var, X, y), axis=0)
+def quadratic(inner_var, outer_var, hess_inner, hess_outer, cross):
+    res = .5 * inner_var @ (hess_inner @ inner_var)
+    res += .5 * outer_var @ (hess_outer @ outer_var)
+    res += outer_var @ cross @ inner_var
+    return res
 
 
 # All datasets must be named `Dataset` and inherit from `BaseDataset`
 class Dataset(BaseDataset):
-    """Hyperparameter optimization with IJCNN1 dataset."""
-    # Name to select the dataset in the CLI and to display the results.
-    name = "ijcnn1"
     """How to add a new problem to the benchmark?
 
     This template dataset is an adaptation of the dataset from the benchopt
     template benchmark (https://github.com/benchopt/template_benchmark/) to
     the bilevel setting.
     """
-
-    install_cmd = 'conda'
-    # List of packages needed to run the dataset. See the corresponding
-    # section in objective.py
-    requirements = ['pip:libsvmdata', 'scikit-learn']
+    # Name to select the dataset in the CLI and to display the results.
+    name = "Template dataset"
 
     # List of parameters to generate the datasets. The benchmark will consider
     # the cross product for each key in the dictionary.
     # Any parameters 'param' defined here is available as `self.param`.
     parameters = {
-        'reg_parametrization': ['exp'],
+        'dim_inner': [10],
+        'dim_outer': [10],
     }
 
     def get_data(self):
@@ -53,43 +80,23 @@ class Dataset(BaseDataset):
         # to `Objective.set_data`. This defines the benchmark's
         # API to pass data.
 
-        X_train, y_train = fetch_libsvm('ijcnn1')
-        X_val, y_val = fetch_libsvm('ijcnn1_test')
-
-        X_train, y_train = jnp.array(X_train), jnp.array(y_train)
-        X_val, y_val = jnp.array(X_val), jnp.array(y_val)
-
-        self.n_samples_inner = X_train.shape[0]
-        self.dim_inner = X_train.shape[1]
-        self.n_samples_outer = X_val.shape[0]
-        self.dim_outer = X_val.shape[1]
+        hess_inner_inner, hess_outer_inner, cross, hess_inner_outer = (
+            generate_matrices(
+                self.dim_inner, self.dim_outer
+            )
+        )
 
         @partial(jax.jit, static_argnames=('batch_size'))
         def f_inner(inner_var, outer_var, start=0, batch_size=1):
-            x = jax.lax.dynamic_slice(
-                X_train, (start, 0), (batch_size, X_train.shape[1])
-            )
-            y = jax.lax.dynamic_slice(
-                y_train, (start, ), (batch_size, )
-            )
-            res = loss(inner_var, outer_var, x, y)
-
-            if self.reg_parametrization == 'exp':
-                res += jnp.dot(jnp.exp(outer_var) * inner_var, inner_var)/2
-            elif self.reg_parametrization == 'lin':
-                res += jnp.dot(outer_var * inner_var, inner_var)/2
-            return res
+            return quadratic(inner_var, outer_var,
+                             hess_inner_inner, hess_outer_inner,
+                             cross)
 
         @partial(jax.jit, static_argnames=('batch_size'))
         def f_outer(inner_var, outer_var, start=0, batch_size=1):
-            x = jax.lax.dynamic_slice(
-                X_val, (start, 0), (batch_size, X_val.shape[1])
-            )
-            y = jax.lax.dynamic_slice(
-                y_val, (start, ), (batch_size, )
-            )
-            res = loss(inner_var, outer_var, x, y)
-            return res
+            return quadratic(inner_var, outer_var, hess_inner_outer,
+                             jnp.zeros_like(hess_outer_inner),
+                             jnp.zeros_like(cross))
 
         f_inner_fb = partial(
             f_inner, batch_size=X_train.shape[0], start=0
